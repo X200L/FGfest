156e3b77d1411aec35c43f30229ee89a0e9c24ea
AlexVorob <kot.voin1@gmail.com>
2025-03-28 10:35:53 +0300

    Update

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // const registerButtons = document.querySelectorAll('.register-button'); // delete this
+
+    // registerButtons.forEach(button => { // delete this
+    //     button.addEventListener('click', function() { // delete this
+    //         alert('Регистрация открыта!'); // delete this
+    //     }); // delete this
+    // }); // delete this
+}); 
